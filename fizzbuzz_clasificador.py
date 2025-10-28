3d599fcc7f1c9822acae3fea1baaa54502c4e1e1
Javier Valenzuela <javiervalenzuela6264@outlook.cl>
2025-10-27 23:17:49 -0300

    conflicto en main

--- fizzbuzz_clasificador.py
+++ fizzbuzz_clasificador.py
@@ -24,7 +24,7 @@ def clasificar_enteros(numeros):
 def mostrar_resultados(fizzbuzz, buzz, fizz, otro):
     print(f"Los múltiplos de 3 son los números: {fizz}")
     print(f"Los múltiplos de 5 son los números: {buzz}")
-    print(f"Los múltiplos tanto de 3 como de 5 son: {fizzbuzz}")
+    print(f"Los múltiplos de 3 y 5 son los siguientes: {fizzbuzz}")
     print(f"Mientras que {otro} son números que no entran en ninguna categoría")
 
 def main():
